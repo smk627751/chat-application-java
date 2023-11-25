7ab37d5aee2a77cfd80cadc0619267d2f46a7f62
smk627751 <smk627751@gmail.com>
2023-11-25 12:43:29 +0530

    updated commit

--- chat application/src/com/smk627751/chatapplication/Server.java
+++ chat application/src/com/smk627751/chatapplication/Server.java
@@ -5,7 +5,6 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
